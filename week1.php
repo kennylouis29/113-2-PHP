a6809fea6dc45b49bd84cc7a503f908ba2f6a751
kennylouis29 <125714859+kennylouis29@users.noreply.github.com>
2025-03-06 15:03:49 +0700

    Add files via upload

---
+++ week1.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Welcome to my first html example</title>
+</head>
+
+<body>
+    Hello HTML
+
+    <?php
+    echo "</br>";
+    echo "Test Php";
+
+    $i = 5;
+    $j = $i + 1;
+    echo $j;
+    ?>
+</body>
+
+</html>
